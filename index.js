0fc32ea077bf860ab60e515427fe2964201f4715
Alexandre Soares <alexandre.desousasoares@gmail.com>
2022-05-30 14:48:27 +0300

    fix

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ app.get("/api/test", async (req, res) => {
 
 // Serve frontend
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "./frontend/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+      path.resolve(__dirname, "./", "frontend", "build", "index.html")
     )
   );
 } else {
